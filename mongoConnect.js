75fcdfb55aab5e5f2d9cd071469755f667f36936
Uliyan <uliyan96@gmail.com>
2018-02-13 12:26:59 +0300

    Add autoReconnect for MongoDB, add ES6 and the 'mongo' object is filling in a separate method

--- mongoConnect.js
+++ mongoConnect.js
@@ -1,21 +1,21 @@
 const MongoClient = require('mongodb').MongoClient,
            assert = require('assert');
 var _database;
-var connect;
+var connection = null;
 var mongo = {};
 const user = 'slotexapp';
 const pwd  = 'slotexapp';
 module.exports = {
     connect: function (callback) {
-      MongoClient.connect( `mongodb://${user}:${pwd}@192.168.0.6:27017/slotex`, function (err, database) {
-        connect            = database;
-        _database           = database.db('slotex');
-        mongo.users         = _database.collection('users');
-        mongo.orders        = _database.collection('orders');
-        mongo.ordersData    = _database.collection('ordersData');
-        mongo.ordersHistory = _database.collection('ordersHistory');
-        return callback (err);
-      });
+      MongoClient.connect( `mongodb://${user}:${pwd}@192.168.0.6:27017/slotex`,
+        {
+          autoReconnect: true
+        },
+        (err, database) => {
+            connection = database;
+            setMongoCollection();
+            return callback (err);
+        });
     },
 
     getDb: function () {
@@ -23,8 +23,22 @@ module.exports = {
     },
 
     closeConnect: function () {
-      connect.close();
+      if (connection !== null) {
+        connection.close();
+      } else {
+        return false;
+      }
     },
 
     mongo
 };
+
+function setMongoCollection() {
+  if (connection !== null) {
+    _database = mongo.db = connection.db('slotex');
+    mongo.users          = _database.collection('users');
+    mongo.orders         = _database.collection('orders');
+    mongo.ordersData     = _database.collection('ordersData');
+    mongo.ordersHistory  = _database.collection('ordersHistory');
+  }
+}
